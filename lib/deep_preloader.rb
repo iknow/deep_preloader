2bf796041446d099b7b47d7b8acd1594da6a19f8
Chris Andreae <chris@bibo.com.ph>
2016-07-22 17:17:28 +0900

    Tidy primary/foreign keys, set inverse instance.

--- lib/deep_preloader.rb
+++ lib/deep_preloader.rb
@@ -48,56 +48,69 @@ module DeepPreloader
 
   # belongs_to: polymorphic: group the models by the foreign_type, look up each grouping as normal.
   def self.preload_polymorphic_association(models, association_reflection, child_spec)
-    models_by_klass_name = models.group_by { |m| m.read_attribute(association_reflection.foreign_type) }
-    models_by_klass_name.each do |klass_name, klass_models|
-      scope = klass_name.constantize.unscoped
+    assoc_name = association_reflection.name
+    models_by_klass = models.group_by { |m| m.association(assoc_name).klass }
+    models_by_klass.each do |klass, klass_models|
+      scope = klass.unscoped
       preload_belongs_to_association(klass_models, association_reflection, scope, child_spec)
     end
   end
 
   def self.preload_belongs_to_association(models, association_reflection, scope, child_spec)
-    assoc = association_reflection.name
     fk = association_reflection.foreign_key
+    pk = association_reflection.active_record_primary_key
 
     fk_ids = models.map { |m| m.read_attribute(fk) }.uniq
-    children = scope.where(id: fk_ids).to_a
+    children = scope.where(pk => fk_ids).to_a
 
     child_spec.preload(children) if child_spec.present?
 
     children_by_id = children.index_by(&:id)
 
     models.each do |m|
-      m.association(assoc).target = children_by_id[m.read_attribute(fk)]
+      target = children_by_id[m.read_attribute(fk)]
+      association = m.association(association_reflection.name)
+      association.target = target
+      association.set_inverse_instance(target) if target
     end
   end
 
   def self.preload_has_one_association(models, association_reflection, scope, child_spec)
-    assoc = association_reflection.name
     fk = association_reflection.foreign_key
+    pk = association_reflection.active_record_primary_key
 
-    children = scope.where(fk => models.map(&:id)).uniq.to_a
+    pk_ids = models.map { |m| m.read_attribute(pk) }
+    children = scope.where(fk => pk_ids).uniq.to_a
 
     child_spec.preload(children) if child_spec.present?
 
     children_by_fk = children.index_by { |rel| rel.read_attribute(fk) }
 
     models.each do |m|
-      m.association(assoc).target = children_by_fk[m.id]
+      target = children_by_fk[m.id]
+      association = m.association(association_reflection.name)
+      association.target = target
+      association.set_inverse_instance(target) if target
     end
   end
 
   def self.preload_has_many_association(models, association_reflection, scope, child_spec)
-    assoc = association_reflection.name
     fk = association_reflection.foreign_key
+    pk = association_reflection.active_record_primary_key
 
-    children = scope.where(fk => models.map(&:id)).to_a
+    pk_ids = models.map { |m| m.read_attribute(pk) }
+    children = scope.where(fk => pk_ids).to_a
 
     child_spec.preload(children) if child_spec.present?
 
     children_by_fk = children.group_by { |rel| rel.read_attribute(fk) }
 
     models.each do |m|
-      m.association(assoc).target = children_by_fk.fetch(m.id, [])
+      targets = children_by_fk.fetch(m.id, [])
+      association = m.association(association_reflection.name)
+      association.loaded!
+      association.target = targets
+      targets.each { |target| association.set_inverse_instance(target) }
     end
   end
 
